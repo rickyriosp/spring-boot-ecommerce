d3d0470e9fb22ef0702491a9980c6a6b0fcd94ed
rickyriosp <riki.rios91@gmail.com>
2023-12-24 14:28:00 -0500

    added cors support

--- src/main/java/com/riosr/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/riosr/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,10 @@ package com.riosr.ecommerce.dao;
 
 import com.riosr.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+//@CrossOrigin    // any origin
+@CrossOrigin("http://localhost:4200")
+//@CrossOrigin({"http://localhost:4200", "http://mywebsite.com:4200"})   // multiple origins
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
