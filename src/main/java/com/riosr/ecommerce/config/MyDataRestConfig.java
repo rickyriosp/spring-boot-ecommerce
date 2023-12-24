b82e4368da609888184484dda63b5735f8210f96
rickyriosp <riki.rios91@gmail.com>
2023-12-24 13:21:04 -0500

    basic crud

---
+++ src/main/java/com/riosr/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,32 @@
+package com.riosr.ecommerce.config;
+
+import com.riosr.ecommerce.entity.Product;
+import com.riosr.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        //RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+
+        HttpMethod[] unsuportedActions = { HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE };
+
+        // disable HTTP methods for Product: PUT, POST, DELETE
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsuportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsuportedActions));
+
+        // disable HTTP methods for ProductCategory: PUT, POST, DELETE
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsuportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsuportedActions));
+    }
+}
