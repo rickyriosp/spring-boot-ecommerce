a0a0ef4c6b5702689c95db5b049d40581e106cc2
rickyriosp <riki.rios91@gmail.com>
2024-01-13 10:27:06 -0500

    search products by category

--- src/main/java/com/riosr/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/riosr/ecommerce/dao/ProductRepository.java
@@ -1,11 +1,17 @@
 package com.riosr.ecommerce.dao;
 
 import com.riosr.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 //@CrossOrigin    // any origin
 @CrossOrigin("http://localhost:4200")
 //@CrossOrigin({"http://localhost:4200", "http://mywebsite.com:4200"})   // multiple origins
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    // Query method -> "SELECT * FROM product WHERE category_id = ?"
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
