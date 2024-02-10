a2022af4943823f85a7cf187ed580787950f1352
rickyriosp <riki.rios91@gmail.com>
2024-02-10 17:03:03 -0500

    order history secure backend bugfix

--- src/main/java/com/riosr/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/riosr/ecommerce/config/SecurityConfig.java
@@ -1,27 +1,25 @@
 package com.riosr.ecommerce.config;
 
 import com.okta.spring.boot.oauth.Okta;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.accept.ContentNegotiationStrategy;
 import org.springframework.web.accept.HeaderContentNegotiationStrategy;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(requests ->
                         requests
-                                // protect endpoint /api/orders
+                                // protect endpoint "/api/orders"
                                 .requestMatchers("/api/orders/**")
                                 .authenticated()
                                 // all other requests
--- src/main/java/com/riosr/ecommerce/dao/OrderRepository.java
+++ src/main/java/com/riosr/ecommerce/dao/OrderRepository.java
@@ -6,11 +6,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 @RepositoryRestResource
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     // Query method
     // SELECT * FROM orders LEFT OUTER JOIN customer ON orders.customer_id = customer.id WHERE customer.email = :email
+    @PreAuthorize("principal.subject == #email")
     Page<Order> findByCustomerEmailOrderByDateCreatedDesc(@Param("email") String email, Pageable pageable);
 }
