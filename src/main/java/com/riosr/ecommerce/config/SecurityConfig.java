4b7c0efdd85339a06fd100004e2addb319fe7b32
rickyriosp <riki.rios91@gmail.com>
2024-04-06 12:33:55 -0400

    updated spring prod profile allowed origin

--- src/main/java/com/riosr/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/riosr/ecommerce/config/SecurityConfig.java
@@ -4,15 +4,15 @@ import com.okta.spring.boot.oauth.Okta;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.accept.ContentNegotiationStrategy;
 import org.springframework.web.accept.HeaderContentNegotiationStrategy;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
